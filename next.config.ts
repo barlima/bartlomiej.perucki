d30d0809b472e8d9f4d2f35aaa241517bcf4ad72
Bartlomiej Perucki <bartekperucki@gmail.com>
2025-10-30 14:51:06 +0100

    feat: init hero

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    deviceSizes: [320, 480, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 1240],
+    qualities: [100],
+  },
 };
 
 export default nextConfig;
