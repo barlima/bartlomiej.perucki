f72d77985bd900376db3f965cb903f172c9258db
Bartlomiej Perucki <bartekperucki@gmail.com>
2026-03-06 21:12:37 +0100

    chore: update copyright year

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="flex justify-center items-center mt-16 w-full">
       <div className="flex flex-col gap-8 justify-center items-center w-full">
         <SocialMedia />
-        <p>Copyright © 2025 Bartłomiej Perucki</p>
+        <p>Copyright © 2026 Bartłomiej Perucki</p>
       </div>
     </footer>
   );
